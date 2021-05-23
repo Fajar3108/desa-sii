7637558fd886ece5a05b3abd3d6520edf86a1ba6
Fajar3108 <maulanafajaribrahim@gmail.com>
2021-05-23 13:07:13 +0700

    Modified: Album API Response

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -17,6 +17,7 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'thumbnail' => $this->galleries()->first()->image ?? "",
             'galleries' => $this->galleries
         ];
     }
