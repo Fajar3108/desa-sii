cbdecfea818ada86ed37671b1e83f02d9cb6b6c8
Fajar3108 <maulanafajaribrahim@gmail.com>
2021-06-24 16:47:53 +0700

    Modified: Order by created_at

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest()->paginate(10);
+        $posts = Post::orderBy('created_at', 'DESC')->paginate(10);
 
         return view('post.index', compact('posts'));
     }
