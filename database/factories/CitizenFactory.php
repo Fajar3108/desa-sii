f1d4c1d20fa8d20fcb5acc3f1537d563697edd34
Fajar3108 <maulanafajaribrahim@gmail.com>
2021-08-31 20:54:07 +0700

    Modified Citizen Seeder

--- database/factories/CitizenFactory.php
+++ database/factories/CitizenFactory.php
@@ -23,16 +23,16 @@ class CitizenFactory extends Factory
     {
         $kk = ['3274567890123456', '3274567893122456', '3274567890223411', '3274567890123252', '3274567890133456', '3274567192123436', '3274527495123456', '3274567890124426'];
         return [
+            'gender' => ($gender = $this->faker->randomElement(array('L','P'))),
             'family_id' => Family::firstOrCreate(['number' => $kk[rand(0, count($kk) - 1)]])->id,
             'nik' => $this->faker->unique()->numberBetween(1111111111111111, 9999999999999999),
-            'name' => $this->faker->name,
+            'name' => $this->faker->name($gender == "L" ? 'male' : 'female'),
             'no_hp' => $this->faker->e164PhoneNumber,
             'birthday' => $this->faker->date,
             'address' => $this->faker->address,
             'rt' => "00" . rand(1, 6),
             'rw' => "00" . rand(1, 6),
             'status' => $this->faker->randomElement(array('mampu','kurang_mampu')),
-            'gender' => $this->faker->randomElement(array('L','P')),
         ];
     }
 }
