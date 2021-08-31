4cb5ec23b978bfbe778f2f743596fd1711568045
Fajar3108 <maulanafajaribrahim@gmail.com>
2021-08-31 21:24:29 +0700

    Modified Ctizen detail Page

--- resources/views/citizen/show.blade.php
+++ resources/views/citizen/show.blade.php
@@ -1,16 +1,55 @@
 @extends('layout.master')
-@section('title', 'Keluarga')
+@section('title', 'Detail Penduduk')
 @section('parentPageTitle', 'Penduduk')
 @section('back-navigation', route('citizen.index'))
 
 @section('content')
 
 <div class="row clearfix">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <h6 class="card-subtitle mb-2 text-primary">{{ $citizen->name }}</h6>
+                <p class="card-text">NIK : {{ $citizen->nik }}</p>
+                <div class="table-responsive">
+                    <table class="table table-sm">
+                        <tr>
+                            <th style="width: 250px">Jenis Kelamin</th>
+                            <td>{{ $citizen->gender == 'L' ? 'Laki-Laki' : 'Perempuan' }}</td>
+                        </tr>
+                        <tr>
+                            <th>Tanggal Lahir</th>
+                            <td>{{ $birthday = date('d M Y' , strtotime($citizen->birthday)) }} ({{ intval(now()->diffInDays($birthday) / 365) }} Tahun)</td>
+                        </tr>
+                        <tr>
+                            <th>No Hp</th>
+                            <td>{{ $citizen->no_hp }}</td>
+                        </tr>
+                        <tr>
+                            <th>Status Ekonomi</th>
+                            <td>{{ Str::replace("_", " ", $citizen->status) }}</td>
+                        </tr>
+                        <tr>
+                            <th>RT / RW</th>
+                            <td>{{ $citizen->rt . " / " . $citizen->rw  }}</td>
+                        </tr>
+                        <tr>
+                            <th>Alamat</th>
+                            <td>{{ $citizen->address  }}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row clearfix">
 
     <div class="col-lg-12">
         <div class="card">
             <div class="header">
-                <h2 style="margin-bottom: 10px !important">Keluarga <span class="text-primary">{{ $citizen->name }}</span></h2>
+                <h2 style="margin-bottom: 10px !important">Keluarga</h2>
                 <p class="mb-0">No KK : <strong>{{ $citizen->family->number }}</strong></p>
                 <p class="mb-0">Jumlah anggota kelarga : {{ $citizens->count() }}</p>
             </div>
@@ -29,7 +68,7 @@
                                 @foreach ($citizens as $citizen)
                                 <tr @if($citizen->id === request()->citizen->id) style="background-color: #cee5ff" @endif>
                                     <td>
-                                        {{ $citizen->name }}
+                                        <a href="{{ route('citizen.show', $citizen->id) }}">{{ $citizen->name }}</a>
                                     </td>
                                     <td>
                                         <span><i class="zmdi"></i>{{ date('d M Y' , strtotime($citizen->birthday)) }}</span>
